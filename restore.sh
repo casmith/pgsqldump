3d487166965dc54838a38fd0a9919fecc0c789c9
Clay Smith <casmith@users.noreply.github.com>
2023-07-21 10:19:48 -0500

    feat: restore image

---
+++ restore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DB_USER=${DB_USER:-${PGSQL_ENV_DB_USER}}
+DB_PASS=${DB_PASS:-${PGSQL_ENV_DB_PASS}}
+DB_NAME=${DB_NAME:-${PGSQL_ENV_DB_NAME}}
+DB_HOST=${DB_HOST:-${PGSQL_ENV_DB_HOST}}
+FILE=${FILE:-${PGSQL_ENV_RESTORE_FILE}}
+
+if [[ ${DB_USER} == "" ]]; then
+	echo "Missing DB_USER env variable"
+	exit 1
+fi
+if [[ ${DB_PASS} == "" ]]; then
+	echo "Missing DB_PASS env variable"
+	exit 1
+fi
+if [[ ${DB_HOST} == "" ]]; then
+	echo "Missing DB_HOST env variable"
+	exit 1
+fi
+
+psql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > /mysqldump/"${DB_NAME}".sql
+
+PGPASSWORD="$DB_PASS" psql --verbose --username=$DB_USER --host=$DB_HOST "${DB_NAME}" < $FILE
