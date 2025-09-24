c179c438d31f05d3736b8ab3c8477c7e1b81eb22
Clay Smith <casmith@users.noreply.github.com>
2025-09-23 23:12:15 -0500

    fix: syntax error in restore

--- restore.sh
+++ restore.sh
@@ -8,19 +8,19 @@ FILE=${FILE:-${PGSQL_ENV_RESTORE_FILE}}
 FLAGS=${FLAGS}
 
 if [[ ${DB_USER} == "" ]]; then
-	echo "Missing DB_USER env variable"
-	exit 1
+  echo "Missing DB_USER env variable"
+  exit 1
 fi
 if [[ ${DB_PASS} == "" ]]; then
-	echo "Missing DB_PASS env variable"
-	exit 1
+  echo "Missing DB_PASS env variable"
+  exit 1
 fi
 if [[ ${DB_HOST} == "" ]]; then
-	echo "Missing DB_HOST env variable"
-	exit 1
+  echo "Missing DB_HOST env variable"
+  exit 1
 fi
-if [[ ! $FLAGS == ""]]; then
-	FLAGS=" $FLAGS"
+if [[ ! $FLAGS == "" ]]; then
+  FLAGS=" $FLAGS"
 fi
 
-PGPASSWORD="$DB_PASS" psql$FLAGS --username=$DB_USER --host=$DB_HOST "${DB_NAME}" < $FILE
+PGPASSWORD="$DB_PASS" psql$FLAGS --username=$DB_USER --host=$DB_HOST "${DB_NAME}" <$FILE
